c7a610dfc88e49302825f273ca0dde1517d91979
Richard Holmes <richard.gordon.holmes@gmail.com>
2025-03-31 10:09:31 +0000

    Link custom themes and plugins

--- install.sh
+++ install.sh
@@ -7,10 +7,6 @@ sudo apt install -y zsh git curl
 # Clone the config repo
 git clone --recurse-submodules https://github.com/Ninteedo/dotfiles.git ~/.config/zsh
 
-# Link configs
-ln -sf ~/.config/zsh/.zshrc ~/.zshrc
-ln -sf ~/.config/zsh/.p10k.zsh ~/.p10k.zsh
-
 # Set Zsh as default shell
 chsh -s $(which zsh)
 
@@ -18,6 +14,12 @@ chsh -s $(which zsh)
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
+# Link configs
+ln -sf ~/.config/zsh/.zshrc ~/.zshrc
+ln -sf ~/.config/zsh/.p10k.zsh ~/.p10k.zsh
+ln -s ~/.config/zsh/custom/themes/* ~/.oh-my-zsh/custom/themes/
+ln -s ~/.config/zsh/custom/plugins/*  ~/.oh-my-zsh/custom/plugins/
 
 # Source the new configuration
 source ~/.zshrc
